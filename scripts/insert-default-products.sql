04a2184c414170deccb4a7120476c571e8c7e2b1
Roman Kuznetsov <a.roman.kuznetsov@gmail.com>
2024-04-20 12:13:18 +0300

    [DB] Add script for inserting default products

---
+++ scripts/insert-default-products.sql
@@ -0,0 +1,2 @@
+INSERT INTO eventuate.product (description, image, product_name, amount, product_quantity) VALUES ('TV', 'http://host/image.png', 'TV', 1000.00, 5);
+INSERT INTO eventuate.product (description, image, product_name, amount, product_quantity) VALUES ('Sofa', 'http://host/image.png', 'Sofa', 500.00, 10);
