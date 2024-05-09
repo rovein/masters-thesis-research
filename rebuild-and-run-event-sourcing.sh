9ff4c6322543dc452c9174403ab2cec7fae266dc
Roman Kuznetsov <a.roman.kuznetsov@gmail.com>
2024-05-09 10:26:34 +0300

    [DevOps] Add separate docker-compose and build/run scripts for Outbox and Event Sourcing services

---
+++ rebuild-and-run-event-sourcing.sh
@@ -0,0 +1,4 @@
+#rebuild all microservices
+mvn clean install -DskipTests -e -T 1C -DtrimStackTrace=false -Dmdep.skip=true -am -Denforcer.skip -Dcheckstyle.skip=true -Dmdep.skip=true
+
+bash ./run-event-sourcing.sh
