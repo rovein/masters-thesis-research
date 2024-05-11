75f5629f6e05bd35189744520c125df124a19003
Roman Kuznetsov <a.roman.kuznetsov@gmail.com>
2024-05-11 23:40:49 +0300

    [Cancel order SAGA] Change logging prefix

--- common/src/main/java/ua/nure/sagaresearch/common/util/LoggingUtils.java
+++ common/src/main/java/ua/nure/sagaresearch/common/util/LoggingUtils.java
@@ -12,7 +12,8 @@ public final class LoggingUtils {
     public static final String ADD_PRODUCT_TO_BASKET_PREFIX = "[OUTBOX Add Product to Basket SAGA]";
     public static final String PLACE_ORDER_PREFIX = "[OUTBOX Place Order SAGA]";
     public static final String CONFIRM_PAYMENT_PREFIX = "[OUTBOX Confirm Payment SAGA]";
-    public static final String CANCEL_ORDER_PREFIX = "[OUTBOX Cancel requested Order SAGA]";
+    public static final String CANCEL_ORDER_PREFIX = "[OUTBOX Cancel Order SAGA]";
+
     public static final String EVENT_SOURCING_ADD_PRODUCT_TO_BASKET_PREFIX = "[Add Product to Basket SAGA] Event Sourcing.";
 
     /**
